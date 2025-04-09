c8cdc65d6de33a6bed8a343af1f24696c227d6b5
Samuel <samuelsouza0802@gmail.com>
2025-04-09 00:33:53 -0300

    Validação Do Formulário e Criação da Nova Nota

--- app/Http/Controllers/MainController.php
+++ app/Http/Controllers/MainController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\Note;
 use App\Models\User;
 use App\services\Operations;
 use Illuminate\Http\Request;
@@ -37,15 +38,51 @@ class MainController extends Controller
 
         echo "$id";
 
-
-
-
     }
 
 
-
     public function newNote(){
-        echo "Criar uma nova nota!";
+
+        //show new note view
+        return view('new_note');
+
+    }
+
+    public function newNoteSubmit(Request $request){
+
+     //Validação dos dados do formulário
+     $regras = [
+       'title' => 'required|min:3|max:200',
+       'text' => 'required|min:3|max:3000',
+     ];
+
+     $feedback = [
+
+     'title.min' => 'O campo deve ter no minimo :min caracteres.',
+     'title.max' => 'O campo deve ter no máxino :max caracteres',
+     'title.required' => 'O campo Titulo é Obrigátorio',
+     'text.min' => 'O campo deve ter no minimo :min caracteres',
+     'text.max' => 'O campo deve ter no máximo :max caracteres',
+     'text.required' => 'O campo Nota é Obrigatório',
+
+     ];
+
+     $request->validate($regras, $feedback);
+
+
+     //Buscar id do user
+     $id = session('user.id');
+
+     //Criar nova nota
+     $nota = new Note();
+     $nota->user_id = $id;
+     $nota->title = $request->input('title');
+     $nota->text = $request->input('text');
+     $nota->save();
+
+     //Redirecionar para a home
+    return redirect()->route('home');
+
 
     }
 
---
+++ resources/views/new_note.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.main_layout')
+@section('content')
+
+<div class="container mt-5">
+    <div class="row justify-content-center">
+        <div class="col">
+
+
+      @include('top_bar')
+
+            <!-- label and cancel -->
+            <div class="row">
+                <div class="col">
+                    <p class="display-6 mb-0">NEW NOTE</p>
+                </div>
+                <div class="col text-end">
+                    <a href="{{ route('home') }}" class="btn btn-outline-danger">
+                        <i class="fa-solid fa-xmark"></i>
+                    </a>
+                </div>
+            </div>
+
+            <!-- form -->
+            <form action="{{ route('newSubmit') }}" method="post">
+                @csrf
+                <div class="row mt-3">
+                    <div class="col">
+                        <div class="mb-3">
+                            <label class="form-label">Titulo</label>
+                            <input type="text" class="form-control bg-primary text-white" name="title" value="{{ old('title') }}">
+
+                            @error('title')
+                                <div class="text-danger">
+                                      {{ $message }}
+                                </div>
+                            @enderror
+                        </div>
+                        <div class="mb-3">
+                            <label class="form-label">Nota</label>
+                            <textarea class="form-control bg-primary text-white" name="text" rows="5" >{{ old('text') }}</textarea>
+
+                            @error('text')
+                            <div class="text-danger">
+                                {{ $message }}
+                            </div>
+
+                            @enderror
+                        </div>
+                    </div>
+                </div>
+                <div class="row mt-3">
+                    <div class="col text-end">
+                        <a href="{{ route('home') }}" class="btn btn-primary px-5"><i class="fa-solid fa-ban me-2"></i>Cancel</a>
+                        <button type="submit" class="btn btn-secondary px-5"><i class="fa-regular fa-circle-check me-2"></i>Save</button>
+                    </div>
+                </div>
+            </form>
+
+        </div>
+    </div>
+</div>
+
+
+
+@endsection
